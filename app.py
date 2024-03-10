3a4349ab9ede3e5d75ddf508bb84e75395c210cb
MartynovaMA <162876744+MartynovaMA@users.noreply.github.com>
2024-03-10 13:06:20 +0300

    Add files via upload
    10 march 2024

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template ,json
+app=Flask(__name__)
+#app=Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def index():
+	return "Welcome!"
+
+@app.route("/login",methods=['POST','GET'])
+def login():
+	if request.method == 'POST':
+		username=request.form.get('username')
+		password=request.form.get('password')
+		#проверка логина и пароля
+		if username and password:
+			return render_template('list.html')
+			#return 'Вы вошли в систему!'
+	else:
+		return render_template('login.html')	
+		
+	
+if __name__=="__main__":
+	from waitress import serve
+	serve(app, host="0.0.0.0", port=8080)	
